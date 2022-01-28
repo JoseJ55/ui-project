07a02269a05b0666e56563b03d83aac716823be2
Jose Jimenez <josejimenez826@gmail.com>
2022-01-27 21:49:15 -0800

    finish brand and header

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import "./brand.css";
+import { google, slack, atlassian, dropbox, shopify } from "./imports";
 
 function Brand() {
   return (
-    <div>
-        Brand
+    <div className='gpt3_brand section_padding'>
+        <div>
+          <img src={google} alt="google" />
+        </div>
+        <div>
+          <img src={slack} alt="slack" />
+        </div>
+        <div>
+          <img src={atlassian} alt="atlassion" />
+        </div>
+        <div>
+          <img src={dropbox} alt="dropbox" />
+        </div>
+        <div>
+          <img src={shopify} alt="shopify" />
+        </div>
     </div>
   );
 }
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import "./header.css";
 
+import people from "../../assets/people.png"
+import ai from "../../assets/ai.png"
+
 function Header() {
   return (
-    <div>
-        Header
+    <div className='gpt3_header section_padding' id="home">
+        <div className='gpt3_header-content'>
+          <h1 className='gradient_text'>Let's Build Something Amazing with GPT-3 OpenAI</h1>
+          <p>Yet bed any for traveling indulgence unpleasing. Not thorughts all exercise blessing. Indulfence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+          
+          <div className='gpt3_header-content_input'>
+            <input type="email" placeholder='Your Email Address' />
+            <button type="button">Get Started</button>
+          </div>
+
+          <div className='gpt3_header-content_people'>
+            <img src={people} alt='people' />
+            <p>1600 people requested access a vist in last 24 hours</p>
+          </div>
+
+        </div>
+        <div className='gpt3_header-image'>
+          <img src={ai} alt="ai" />
+        </div>
     </div>
   );
 }
